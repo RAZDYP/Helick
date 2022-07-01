d6ad5defa535df6301d4246f5955a127b0d6c4fd
NoisyBotDude <ashishsarmah11@gmail.com>
2022-07-01 21:18:31 +0530

    feat: made the home page responsive

--- src/components/Home/Info.jsx
+++ src/components/Home/Info.jsx
@@ -1,16 +1,22 @@
 import React from "react"
+import "./info.css"
 
 export default function Info() {
     return (
         <div className="info">
             <div className="info-container">
                 <div className="info-text">
-                    <h1>Helick</h1>
-                    <p>Helick is a new way to connect with your customers.
-                        We are a digital marketing company that specializes in
-                        creating a unique and engaging online experience for your
-                        customers.
-                    </p>
+                    <h1>Helick! The new Definition of Speed, Catering to the technological needs of masses</h1>
+                </div>
+                <div className="info-description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                    mollit anim id est laborum.
+                </div>
+                <div className="info-link">
+                    <button className="info-button">Start Shopping</button>
                 </div>
             </div>
         </div>
