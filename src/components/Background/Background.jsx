06404f50e54a0fcdb6fd9d791264c782b04eb909
NoisyBotDude <ashishsarmah11@gmail.com>
2022-07-04 22:38:45 +0530

    feat: added, background, picture, responsive
    Co-authored-by: rajdeeprajdeep03@gmail.com <rajdeeprajdeep03@gmail.com>

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import "./background.css"
+import vector from "../../images/vector.png"
+import rectangle from "../../images/small-rectangle.png"
+
+export default function Background() {
+    return (
+        <div className="background">
+            <img className="background-vector" src={vector} alt=""/>
+            {/* <img className="background-rectangle rectangle-1" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-2" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-3" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-4" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-5" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-6" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-7" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-8" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-9" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-10" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-11" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-12" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-13" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-14" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-15" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-16" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-18" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-19" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-20" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-21" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-22" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-23" src={rectangle} alt=""/>
+            <img className="background-rectangle rectangle-24" src={rectangle} alt=""/> */}
+        </div>
+    )
+}
--- src/sections/Home/Home.js
+++ src/sections/Home/Home.js
@@ -1,19 +1,20 @@
 import React from "react"
-import Info from "../../components/Home/Info"
-import Illustration from "../../components/Home/Illustration"
+import Info from "../../components/Info/Info"
 import Search from "../../components/Search/Search"
+import Illustration from "../../components/Illustration/Illustration"
 import { Row, Col, Container } from "react-bootstrap"
+import "./home.css"
 
 export default function Home() {
     return (
         <Container fluid>
             <Row>
                 <Col md={6}> <Info /></Col>
-                <Col md={6}><Illustration /> </Col>
+                <Col md={6}> <Illustration /></Col>
             </Row>
-            <Row>
+            {/* <Row>
                 <Col md={12}> <Search /> </Col>
-            </Row>
+            </Row> */}
         </Container>
     )
 }
