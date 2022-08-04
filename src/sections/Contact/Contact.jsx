544c8978cc40113fc24e9641b6b460315394dcfc
rajdeep <rajdeeprajdeep03@gmail.com>
2022-08-04 13:48:01 +0530

    contact us page updated

---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,71 @@
+import React from "react"
+import { Row, Col, Container } from "react-bootstrap"
+import { Link } from 'react-router-dom'
+import "./Contact.css"
+
+
+
+
+export default function Contact() {
+    return (
+
+
+
+
+
+
+
+
+        // {/* <!-- Web Fonts  */}
+        // {/* ================================================== --> */}
+        // <Link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+        //     rel="stylesheet" />
+        // <Link href="https://fonts.googleapis.com/css?family=Pinyon+Script" rel="stylesheet">
+        //     <Link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet"/>
+
+        //         {/* <!-- CSS */}
+        //         {/* ================================================== --> */}
+                // <Link rel="stylesheet" href="../css/bootstrap.min.css" />
+                // <Link rel="stylesheet" href="../css/font-awesome.min.css" />
+                // <Link rel="stylesheet" href="../css/jquery.webui-popover.css" />
+                // <Link rel="stylesheet" href="css/style.css" /> 
+                // <Link rel="stylesheet" href="../css/jquery.fancybox.min.css" />
+                // <Link rel="stylesheet" href="../css/login.css" />
+
+
+
+        <div className="container">
+            <div className="login">
+                <div className="login-description">
+                    <h1>Login</h1>
+                    <p className="login-description--subheader">Lorem Ipsum Statement</p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget iaculis mi. Proin vestibulum
+                        massa
+                        sit amet metus tincidunt, et suscipit ipsum pellentesque. Aliquam facilisis lacinia risus ac
+                    </p>
+                </div>
+                <div className="login-info">
+                    <div className="login-info--input">
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <textarea rows="4" cols="50" placeholder="Type your query here."></textarea>
+
+                    </div>
+                    <div className="login-info--button">
+                        <button>Send</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+                                // {/* <script src="../js/jquery.min.js"></script>
+                                // <script src="../js/popper.min.js"></script>
+                                // <script src="../js/bootstrap.min.js"></script>
+     )                           // <script src="../js/plugins.js"></script> */}
+
+
+                                
+
+}
+
