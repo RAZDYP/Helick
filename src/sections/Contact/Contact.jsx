e7918aee1710681d79c21e1dd1b0c2f52eee83c2
rajdeep <rajdeeprajdeep03@gmail.com>
2022-08-04 17:33:41 +0530

    contact us page updated

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -2,6 +2,12 @@ import React from "react"
 import { Row, Col, Container } from "react-bootstrap"
 import { Link } from 'react-router-dom'
 import "./Contact.css"
+import logo from "../../images/logo.png"
+import email from "../../images/email.png"
+import address from "../../images/address.png"
+import phone from "../../images/phone.png"
+
+
 
 
 
@@ -25,30 +31,59 @@ export default function Contact() {
 
         //         {/* <!-- CSS */}
         //         {/* ================================================== --> */}
-                // <Link rel="stylesheet" href="../css/bootstrap.min.css" />
-                // <Link rel="stylesheet" href="../css/font-awesome.min.css" />
-                // <Link rel="stylesheet" href="../css/jquery.webui-popover.css" />
-                // <Link rel="stylesheet" href="css/style.css" /> 
-                // <Link rel="stylesheet" href="../css/jquery.fancybox.min.css" />
-                // <Link rel="stylesheet" href="../css/login.css" />
+        // <Link rel="stylesheet" href="../css/bootstrap.min.css" />
+        // <Link rel="stylesheet" href="../css/font-awesome.min.css" />
+        // <Link rel="stylesheet" href="../css/jquery.webui-popover.css" />
+        // <Link rel="stylesheet" href="css/style.css" /> 
+        // <Link rel="stylesheet" href="../css/jquery.fancybox.min.css" />
+        // <Link rel="stylesheet" href="../css/login.css" />
 
 
 
         <div className="container">
             <div className="login">
                 <div className="login-description">
-                    <h1>Login</h1>
-                    <p className="login-description--subheader">Lorem Ipsum Statement</p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget iaculis mi. Proin vestibulum
-                        massa
-                        sit amet metus tincidunt, et suscipit ipsum pellentesque. Aliquam facilisis lacinia risus ac
-                    </p>
+                    <h1> Get in touch </h1>
+                    <div className="contact-details">
+                        <div className="phone-details all-details">
+                            <img src={phone} alt="logo" width="40" height="40" />
+                            <h5>Phone</h5>
+                            <div className="numbers">
+                                <h6>Phone : +917002043811 </h6>
+                                <h6>Phone : +918486870444 </h6>
+                            </div>
+
+                        </div>
+                        <div className="address-details all-details">
+                            <img src={address} alt="logo" width="40" height="40" />
+                            <h5>Address</h5>
+
+                            <div className="real-address">
+                                <h6>Rampur,Near KMK School</h6>
+                                <h6>Hojai ,Assam 782435</h6>
+                            </div>
+                        </div>
+                        <div className="email-details all-details">
+                            <img src={email} alt="logo" width="40" height="40" />
+                            <h5>Email</h5>
+                            <div>
+                                <h6>  info@helick.in</h6>
+                                <h6>abhironi1@hotmail.com</h6>
+                            </div>
+
+                        </div>
+
+                    </div>
                 </div>
                 <div className="login-info">
+                    <div className="help-message">
+                        <h3> How can we help?</h3>
+                        <p>Send us a message!</p>
+                    </div>
                     <div className="login-info--input">
                         <input type="text" placeholder="Name" />
                         <input type="email" placeholder="Email" />
+                        <input type="tel" placeholder="Phone Number" />
                         <textarea rows="4" cols="50" placeholder="Type your query here."></textarea>
 
                     </div>
@@ -59,13 +94,13 @@ export default function Contact() {
             </div>
         </div>
 
-                                // {/* <script src="../js/jquery.min.js"></script>
-                                // <script src="../js/popper.min.js"></script>
-                                // <script src="../js/bootstrap.min.js"></script>
-     )                           // <script src="../js/plugins.js"></script> */}
+        // {/* <script src="../js/jquery.min.js"></script>
+        // <script src="../js/popper.min.js"></script>
+        // <script src="../js/bootstrap.min.js"></script>
+    )                           // <script src="../js/plugins.js"></script> */}
 
 
-                                
+
 
 }
 
